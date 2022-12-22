67fafde6453261e2487c05fe31b094b9abdadb28
“prongbang” <prongbang@gmail.com>
2022-12-22 13:33:21 +0700

    Initial Commit

---
+++ SignatureBiometricSwiftExample/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SignatureBiometricSwiftExample
+//
+//  Created by M on 22/12/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SignatureBiometricSwiftExample/SignatureBiometricSwiftExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignatureBiometricSwiftExampleApp.swift
+//  SignatureBiometricSwiftExample
+//
+//  Created by M on 22/12/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct SignatureBiometricSwiftExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
